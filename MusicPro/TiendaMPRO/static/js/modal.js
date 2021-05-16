d8c566eca005614192fe3b7f9ec5515724e8698b
Fabian Cuevas <fa.cuevasg@duocuc.cl>
2021-05-16 00:23:06 -0400

    Home 1.0

---
+++ MusicPro/TiendaMPRO/static/js/modal.js
@@ -0,0 +1,7 @@
+var modal = document.getElementById('miModal');
+var flex = document.getElementById('flex');
+var cerrar = document.getElementById('close');
+
+cerrar.addEventListener('click', function(){
+    modal.style.display = 'none';
+});
