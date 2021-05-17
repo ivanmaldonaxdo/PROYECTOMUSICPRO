6d5c009be097843d1e93809edcdcc80f9b8eb7cb
Fabian Cuevas <fa.cuevasg@duocuc.cl>
2021-05-17 16:10:46 -0400

    Bruh

--- MusicPro/TiendaMPRO/static/js/modal.js
+++
@@ -1,7 +0,0 @@
-var modal = document.getElementById('miModal');
-var flex = document.getElementById('flex');
-var cerrar = document.getElementById('close');
-
-cerrar.addEventListener('click', function(){
-    modal.style.display = 'none';
-});
