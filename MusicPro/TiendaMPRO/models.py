ed875a483d3e2cd6b2ff40953782d8fa48866894
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2021-05-20 02:01:51 -0400

    Modelos Categ,Subcateg y Tipo producto hechos

--- MusicPro/TiendaMPRO/models.py
+++ MusicPro/TiendaMPRO/models.py
@@ -3,3 +3,23 @@ from django.utils import timezone
 
 
 # Create your models here.
+class Categoria(models.Model):
+    """Model definition for ."""
+    categ_name = models.TextField(min_length=6,max_length=30,verbose_name="Categoria")
+    sub_categ = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
+ 
+    def __str__(self):
+        return self.categ_name
+
+class SubCategoria(models.Model):
+    sub_categ_name = models.TextField(min_length=6,max_length=30,verbose_name="Sub-Categoria")
+    tipo_prod = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.sub_categ_name
+
+class TipoProducto(models.Model):
+    tipo_name = models.TextField(min_length=6,max_length=30,verbose_name="Tipo de Producto")
+    # TODO: Define fields here
+    def __str__(self):
+         return  self.tipo_name,
+
