11407e4d8676db65251962d74e462de8d0ebbc04
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2021-05-20 15:19:15 -0400

    Modelo mejorado

--- MusicPro/MusicPro/urls.py
+++ MusicPro/MusicPro/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include,url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('TiendaMPRO.urls')),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- MusicPro/TiendaMPRO/models.py
+++ MusicPro/TiendaMPRO/models.py
@@ -1,25 +1,39 @@
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
+from django.core.validators import MinLengthValidator
+
 
 
 # Create your models here.
 class Categoria(models.Model):
-    """Model definition for ."""
-    categ_name = models.TextField(min_length=6,max_length=30,verbose_name="Categoria")
-    sub_categ = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
- 
+    categ_name = models.CharField(validators=[MinLengthValidator(3)],max_length=40,verbose_name="Categoria") 
     def __str__(self):
         return self.categ_name
 
 class SubCategoria(models.Model):
-    sub_categ_name = models.TextField(min_length=6,max_length=30,verbose_name="Sub-Categoria")
-    tipo_prod = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
+    sub_categ_name = models.CharField(validators=[MinLengthValidator(3)],max_length=40,verbose_name="Sub-Categoria")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE,verbose_name="Categoria")
+
     def __str__(self):
         return self.sub_categ_name
 
 class TipoProducto(models.Model):
-    tipo_name = models.TextField(min_length=6,max_length=30,verbose_name="Tipo de Producto")
-    # TODO: Define fields here
-    def __str__(self):
-         return  self.tipo_name,
+    tipo_name = models.CharField(validators=[MinLengthValidator(3)],max_length=40,verbose_name="Tipo de Producto")
+    sub_categ = models.ForeignKey(SubCategoria, on_delete=models.CASCADE,verbose_name="SubCategoria")
 
+    def __str__(self):
+         return  self.tipo_name
+
+# def ruta(filename):
+#     return 'images/producto/{0}'+ '_'+'{1}'.format(pr.pk,filename)
+class Producto(models.Model):
+    nom_prod = models.CharField(validators=[MinLengthValidator(3)], max_length=40,verbose_name="Nombre de Producto")
+    tipo_prod = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
+    descripcion = models.TextField(max_length=1500,null=True,blank=True)
+    precio = models.FloatField(default=0.0)
+    imagen = models.ImageField(upload_to="images/producto",null=True,blank=True)
+    
+    def __str__(self):
+        return self.nom_prod
+
+    
