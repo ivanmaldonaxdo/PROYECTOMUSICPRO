cdc3ef5f0b0399855b5625544d912111e199a3c4
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2021-06-23 22:35:38 -0400

    eventos de on_change inservibles

--- MusicPro/TiendaMPRO/static/js/cbo_anidados.js
+++ MusicPro/TiendaMPRO/static/js/cbo_anidados.js
@@ -1,22 +1,14 @@
 $(document).ready(inicio)
-
 function inicio() {
-    $(function() {
-		$('#subcategoria').chained('#categoria');
-        $('#tp_producto').chained('#subcategoria');
-	});
-
+    //#region ANIOANDO COMBOBOXES
     $(function () {
-        $("#categoria, #subcategoria, #tp_producto").on('change',function () {
-            // alert("CBO CAMBIADO")
-            // $("#filtro").submit();
-           var v_categ=$("#categoria").val();
-           var v_subcateg=$("#categoria").val();
-           var v_tprod=$("#categoria").val();
-           console.log("Categoria : ",v_categ," -subcategoria : ", v_subcateg," -tipo producto : ",v_tprod);
-           $("#filtro").submit();
-
-        });
+        $('#subcategoria').chained('#categoria');
+        $('#tp_producto').chained('#subcategoria');
     });
-
+    //#endregion
+    //#region SUBMIT AL CAMBIAR DATO DE ENTRADA
+        // $('#buscar').on('change',function () {
+        //     $('filtro').submit();
+        // });
+    //#endregion
 };
