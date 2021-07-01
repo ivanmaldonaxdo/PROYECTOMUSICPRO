829270313d7c0d968a9e1ae13954671015101b46
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2021-06-30 23:23:25 -0400

    selenium arranca

--- MusicPro/TiendaMPRO/tests.py
+++ MusicPro/TiendaMPRO/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 import time
 # Create your tests here.
 class Hosttest(LiveServerTestCase):
     def testhomepage(self):
-        driver = webdriver.Chrome()
-        driver.get('http://127.0.0.1:8000/login/')
-        assert " hello , world" in driver.title
+        options = options()
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get(self.live_server_url)
+        driver.get('http://127.0.0.1:8000')
+        assert "hello , world" in driver.title
     # pass
