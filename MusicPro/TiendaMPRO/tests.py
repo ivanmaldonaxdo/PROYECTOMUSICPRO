78264f689055f66e759c57869aacb0e1bd71c427
Ivan Maldonado Gonzalez <iv.maldonadog@alumnos.duoc.cl>
2021-06-30 20:57:53 -0400

    test con selenium

--- MusicPro/TiendaMPRO/tests.py
+++ MusicPro/TiendaMPRO/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import time
 # Create your tests here.
+class Hosttest(LiveServerTestCase):
+    def testhomepage(self):
+        driver = webdriver.Chrome()
+        driver.get('http://127.0.0.1:8000/login/')
+        assert " hello , world" in driver.title
+    # pass
